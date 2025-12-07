67d8fd8ffe56bc74d703b85e0de01cf9efabb7cf
arpitawadekar2005-hub <arpitawadekar2005@gmail.com>
2025-12-07 22:01:36 +0530

    Update streamlit_app.py

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -192,22 +192,23 @@ def render_prediction_ui(image_bytes, result_raw, btn_key: str):
             <div class="pred-item">
                 <div class="v"><b>PLANT : {data.get('plant', '—')}</b></div>
             </div>
-    
+        
             <div class="pred-item">
                 <div class="v"><b>DISEASE : {data.get('disease', '—')}</b></div>
             </div>
-    
+        
             <div class="pred-item">
                 <div class="v"><b>PESTICIDE : {data.get('pesticide', '—')}</b></div>
             </div>
-    
+        
             <div class="pred-item">
                 <div class="v"><b>DOSE (per 100 ml) : {data.get('dose', '0')} ml</b></div>
             </div>
         </div>
         """,
         unsafe_allow_html=True,
-    )
+        )
+
 
         # Actions
         st.markdown("<div class='actions'>", unsafe_allow_html=True)
